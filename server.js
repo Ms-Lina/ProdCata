f7202e39d80cf7b8026044be8a81e52b2f5dee07
Ms-Lina <linamis8008@gmail.com>
2025-07-20 18:59:39 +0200

    updated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require('./src/config/db');
 const productRoutes = require('./src/routes/productRoutes');
 const categoryRoutes = require('./src/routes/categoryRoutes');
 // const authRoutes = require('./src/routes/authRoutes');
+const swaggerApp = require('./src/app');
 
 const app = express();
 connectDB();
@@ -16,7 +17,7 @@ app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
-
+app.use('/api-docs', swaggerApp);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
